c6fd49335e2a0033615e703a3216c73516b8d9de
Abi أب <el@elabee.me>
2025-12-17 16:58:41 +0700

    Add JavaScript dependencies to allow symfony plugin in tests (#23)
    * Add JavaScript dependencies to allow symfony plugin in tests
    
    * Upload jUnit report to codecov
    
    * Qodana cloud token

--- src/test/kotlin/me/elabee/idea/drupal/DrupalTestCase.kt
+++ src/test/kotlin/me/elabee/idea/drupal/DrupalTestCase.kt
@@ -4,27 +4,20 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.jetbrains.php.drupal.DrupalVersion
 import com.jetbrains.php.drupal.settings.DrupalDataService
 
-abstract class DrupalTestCase(protected val enableDrupalService: Boolean = true) : BasePlatformTestCase() {
-
+abstract class DrupalTestCase : BasePlatformTestCase() {
     override fun setUp() {
         super.setUp()
 
-        if (enableDrupalService) {
-            DrupalDataService.getInstance(project).enable()
-        }
-    }
-
-    override fun getTestDataPath(): String {
-        return "src/test/testData/"
-    }
-
-    fun DrupalDataService.enable() {
-        state = DrupalDataService.State(
+        DrupalDataService.getInstance(project).state = DrupalDataService.State(
             true,
             "${testDataPath}drupal",
             DrupalVersion.NINE.number,
             false,
             false,
         )
+    }
+
+    override fun getTestDataPath(): String {
+        return "src/test/testData/"
     }
 }
---
+++ src/test/kotlin/me/elabee/idea/drupal/template/DrupalTwigCompletionTest.kt
@@ -0,0 +1,13 @@
+package me.elabee.idea.drupal.template
+
+import com.intellij.testFramework.EditorTestUtil
+import com.jetbrains.twig.TwigFileType
+import me.elabee.idea.drupal.DrupalComponentTestCase
+
+class DrupalTwigCompletionTest : DrupalComponentTestCase() {
+    fun `test component completion`() {
+        myFixture.configureByText(TwigFileType.INSTANCE, "{{ include('${EditorTestUtil.CARET_TAG}') }}")
+        myFixture.completeBasic()
+        assertContainsElements(myFixture.lookupElementStrings!!, COMPONENTS)
+    }
+}
